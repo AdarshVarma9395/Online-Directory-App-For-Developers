cb0a452df3f21013605c8cf770d0508c22ae0d64
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-05-27 03:51:14 +0530

    Database

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path("", projects, name='projects'),
     path("single-project/<str:pk>/", singleProject, name='single-project'),
+    path('create-project/', createProject, name='create-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import ProjectForm
+
 
 def projects(request):
-    context = {}
+    projects = Project.objects.all()
+    context = {'projects':projects}
     return render(request, "projects/projects.html", context)
 
 def singleProject(request, pk):
-    context = {}
+    project = Project.objects.get(id=pk)
+    context = {'projects':project}
     return render(request, "projects/single-project.html", context)
+
+def createProject(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form}
+    return render(request, 'projects/project_form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+            
+    context = {'form':form}
+    return render(request, 'projects/project-form.html', context)
