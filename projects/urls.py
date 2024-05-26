c5fb676e93551c5ae41887a541b2f972fdc519b3
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-05-27 04:59:04 +0530

    CRUD

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
     path("", projects, name='projects'),
     path("single-project/<str:pk>/", singleProject, name='single-project'),
+
     path('create-project/', createProject, name='create-project'),
+    path('update-project/<str:pk>/', updateProject, name='update-project'),
+    path('delete-project/<str:pk>/', deleteProject, name='delete-project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -33,4 +33,14 @@ def updateProject(request, pk):
             return redirect('projects')
             
     context = {'form':form}
-    return render(request, 'projects/project-form.html', context)
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object':project}
+    return render(request, 'projects/delete_template.html', context)
+
