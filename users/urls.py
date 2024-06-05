3af8609e9d38f22b227c94180452bd2333102144
AdarshVarma9395 <adarshvarma1999@gmail.com>
2024-06-05 18:25:20 +0530

    User-Profile

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', profiles, name = 'profiles'),
+    path('profile/<str:pk>/', userProfile, name = 'user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 def profiles(request):
-    context = {}
+    profiles = Profile.objects.all()
+    context = {'profiles':profiles}
     return render(request, 'users/profile.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile':profile}
+    return render(request, 'users/user-profile.html', context)
